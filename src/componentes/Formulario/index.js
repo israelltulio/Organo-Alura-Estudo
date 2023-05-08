84df9232e902a7918923512ca0cbe5033dd602a7
Israell Túlio ® <121645979+israelltulio@users.noreply.github.com>
2023-05-08 18:13:38 -0400

    Organo Alura Update
    Organo Alura Update de Versão 2.0

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,89 +1,79 @@
 import { useState } from 'react'
 import Botao from '../Botao'
-import CampoTexto from '../CampoTexto'
+import Campo from '../Campo'
 import ListaSuspensa from '../ListaSuspensa'
-import './Formulario.css'
+import './formulario.css'
 
-const Formulario = (props) => {
+const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
 
     const [nome, setNome] = useState('')
-    const [cpf, setCpf] = useState('')
-    const [data, setData] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
 
-    const aoSalvar = (evento) => {
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
+
+    const aoSubmeter = (evento) => {
         evento.preventDefault()
-        props.aoColaboradorCadastrado({
+        console.log('form enviado', nome, cargo, imagem, time )
+        aoCadastrar({
             nome,
-            cpf,
-            data,
             cargo,
             imagem,
             time
         })
-        setNome('')
-        setCpf('')
-        setData('')
-        setCargo('')
-        setImagem('')
-        setTime('')
     }
 
     return (
-        <section className="formulario">
-            <form onSubmit={aoSalvar}>
-                <h2>Preencha os dados para criar um card do colaborador</h2>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite o nome"
+        <section className="formulario-container">
+            <form className="formulario" onSubmit={aoSubmeter}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <Campo
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite seu nome '
                     valor={nome}
-                    aoAlterado={valor => setNome(valor)}
-                />
-
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="CPF" 
-                    placeholder="Digite o CPF"
-                    valor={cpf}
-                    aoAlterado={valor => setCpf(valor)}
-                />
-
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Data de Nascimento" 
-                    placeholder="Digite a data de nascimento"
-                    valor={data}
-                    aoAlterado={valor => setData(valor)}
-                />
-
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Cargo" 
-                    placeholder="Digite o cargo"
+                    aoAlterado={valor => setNome(valor)}/>
+                <Campo
+                    obrigatorio={true}
+                    label='Cargo' 
+                    placeholder='Digite seu cargo '
                     valor={cargo}
-                    aoAlterado={valor => setCargo(valor)}
-                />
-
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Imagem" 
-                    placeholder="Digite o endereço da imagem"
-                    valor={imagem}
-                    aoAlterado={valor => setImagem(valor)}
-                />
-
+                    aoAlterado={valor => setCargo(valor)}/>
+                <Campo 
+                    label='Imagem' 
+                    placeholder='Informe o endereço da imagem '
+                    aoAlterado={valor => setImagem(valor)}/>
                 <ListaSuspensa 
                     obrigatorio={true}
-                    label="Time" 
-                    itens={props.times}
+                    label='Times'
+                    items={times} 
                     valor={time}
-                    aoAlterado={valor => setTime(valor)}
+                    aoAlterado={valor => setTime(valor)}/>
+                <Botao texto='Criar Card' />
+            </form>
+            <form className="formulario" onSubmit={(evento) => {
+                evento.preventDefault()
+                cadastrarTime({ nome: nomeTime, cor: corTime })
+            }}>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <Campo
+                    obrigatorio
+                    label='Nome'
+                    placeholder='Digite o nome do time'
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}
                 />
-
-                <Botao> Criar Card </Botao>
+                <Campo
+                    obrigatorio
+                    type='color'
+                    label='Cor' 
+                    placeholder='Digite a cor do time'
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}
+                />
+                <Botao texto='Criar Novo Time' />
             </form>
         </section>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,23 +1,25 @@
-import Colaborador from '../Colaborador/Colaborador'
-import './Time.css'
+import Colaborador from '../Colaborador'
+import './time.css'
+import hexToRgba from 'hex-to-rgba'
 
-const Time = (props) => {
-
-    const css = { backgroundColor: props.corSecundaria }
-    const borderColor = {borderColor: props.corPrimaria}
-
-    return ( (props.colaboradores.length > 0) &&
-        <section className='time'style={css}>
-            <h3 style={borderColor}>{props.nome}</h3>
-
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
+    return (
+        colaboradores.length > 0 &&
+        <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-cor' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} 
-                    nome={colaborador.nome}
-                    cpf={colaborador.cpf}
-                    data={colaborador.data}
-                    cargo={colaborador.cargo}
-                    imagem={colaborador.imagem}
-                />)}
+                {colaboradores.map((colaborador, indice) => {
+                    return (
+                        <Colaborador 
+                            key={indice}
+                            colaborador={colaborador}
+                            corDeFundo={time.cor}
+                            aoDeletar={aoDeletar}
+                            aoFavoritar={aoFavoritar}
+                        />
+                    );
+                })}
             </div>
         </section>
     )
